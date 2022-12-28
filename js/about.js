c6018b954a3de0210fab6e0605c33b8f02c3704e
Rodrigo Conde Clavijo <rodrigo.conde@fing.edu.uy>
2022-12-28 20:16:00 -0300

    WIP

--- js/about.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(()=>{
-
-	//intro animation
-	$("p").css("animation-duration", "2s").addClass("animated fadeInUp");
-	$("img").css("animation-duration", "2s").addClass("animated fadeInDown");
-	$(".xs-3").css("animation-delay", "2.5s").css("animation-duration", "1s").addClass("animated flash");
-});
